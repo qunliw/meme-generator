54cc91856adb5c1932fb2a336164549050251568
qunliw <62570840+qunliw@users.noreply.github.com>
2020-04-10 03:36:26 -0500

    Updates

---
+++ precache-manifest.474e95f09aaf22dd4a63fd980aee6d07.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "7f3de3cb4e9de96e082b328ceb7ed9d1",
+    "url": "/meme-generator/index.html"
+  },
+  {
+    "revision": "296c64d30dfa10f8d457",
+    "url": "/meme-generator/static/js/2.6ed032c2.chunk.js"
+  },
+  {
+    "revision": "4ba1a1c11c57ed15aa68008707a3d6b4",
+    "url": "/meme-generator/static/js/2.6ed032c2.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "63bdbd3a9972f00daef7",
+    "url": "/meme-generator/static/js/main.55ecaa17.chunk.js"
+  },
+  {
+    "revision": "47ef1aee987066625a71",
+    "url": "/meme-generator/static/js/runtime-main.bc1ff169.js"
+  }
+]);
